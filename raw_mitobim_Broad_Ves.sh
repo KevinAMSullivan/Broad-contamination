0a57e2972a5d6e9081f0c09707dad48d9a35776a
Kevin Sullivan <kev.am.sullivan@gmail.com>
2018-03-21 11:27:45 -0500

    With added sorting, bamtofastq, and pear steps. Plus changed input raw data to mira

--- raw_mitobim_Broad_Ves.sh
+++ raw_mitobim_Broad_Ves.sh
@@ -2,7 +2,7 @@
 #$ -V
 #$ -cwd
 #$ -S /bin/bash
-#$ -N Ves_Broad_mitobim
+#$ -N Broad_mitobim
 #$ -o $JOB_NAME.o$JOB_ID
 #$ -e $JOB_NAME.e$JOB_ID
 #$ -q omni
@@ -10,6 +10,7 @@
 #$ -P quanah
 
 #This file will process raw Illumina data using Trimmomatic.  This will be followed by mapping to a reference mitochondrial genome using MIRA4 to create a new mitochondrial genome assembly.
+module purge #previously got errors concerning incompatible module loads so this is to get a fresh restart 
 
 BASEDIR=/lustre/scratch/kevsulli/Mitobim/Broad_troubleshooting
 WORKDIR=$BASEDIR/output
@@ -17,7 +18,7 @@ WORKDIR=$BASEDIR/output
 mkdir $WORKDIR
 cd $WORKDIR
 
-REFGENOME=Pipistrellus_kuhlii.fas  	# your reference genome for the assembly
+REFGENOME=Pipistrellus_kuhlii.fasta  	# your reference genome for the assembly
 REF_HOME=/lustre/scratch/kevsulli/Mitobim/Broad_troubleshooting/reference_genomes	#the location of your reference genome
 
 
@@ -25,7 +26,7 @@ RAW_READS_HOME=/lustre/scratch/kevsulli/Mitobim/Broad_troubleshooting/data   #th
 #mkdir $BASEDIR/data_raw
 #UNZIPPED_RAW_HOME=$BASEDIR/data_raw
 mkdir $BASEDIR/support_files
-SUPPORT_FILES=$BASEDIR/support_files	#where the support files like the adapter sequences will be located.
+SUPPORT_FILES=$BASEDIR/support_files	#where the .conf files will be located.
 
 ######
 #set up alias' for major programs
@@ -34,13 +35,13 @@ SUPPORT_FILES=$BASEDIR/support_files	#where the support files like the adapter s
 RAY_SOFTWARE=/lustre/work/daray/software
 VCFTOOLS_HOME=/lustre/work/daray/software/vcftools_0.1.12b/bin
 MIRA_HOME=/lustre/scratch/kevsulli/Apps/Mitobim/mira-4.0.2
-MITOBIM=/lustre/work/daray/software
-MIRA=/opt/apps/nfs/intel/impi/mira/4.0.2/bin
+MITOBIM=/lustre/scratch/kevsulli/Apps/Mitobim/MITObim-master
 
 
-for RAW_READ_FILE in $RAW_READS_HOME/*_U0023_10pct.fastq
+
+for RAW_READ_FILE in $RAW_READS_HOME/*_10pct.bam
 do
-	SAMPLE_ID=$(basename $RAW_READ_FILE _U0023_10pct.fastq)
+	SAMPLE_ID=$(basename $RAW_READ_FILE _10pct.bam)
 #Unzip the raw reads into the processed_reads directory
 #	gunzip -c $RAW_READS_HOME/$SAMPLE_ID"_L001_R1_001.fastq.gz" >$UNZIPPED_RAW_HOME/$SAMPLE_ID"_R1.fastq"
 #	gunzip -c $RAW_READS_HOME/$SAMPLE_ID"_L001_R2_001.fastq.gz" >$UNZIPPED_RAW_HOME/$SAMPLE_ID"_R2.fastq"
@@ -48,17 +49,28 @@ do
 mkdir $WORKDIR/$SAMPLE_ID
 cd $WORKDIR/$SAMPLE_ID
 
+load intel/17.3.191
+load htslib/1.3.2-intel
+module load samtools/1.3.1-intel
+samtools sort -n $SAMPLE_ID"_10pct.bam >"$SAMPLE_ID"_10pct.bam.qsort"
+
+module load bedtools
+bedtools bamtofastq -i $SAMPLE_ID"_10pct.bam.qsort -fq "$SAMPLE_ID"_10pct_R1.fastq -fq2 "$SAMPLE_ID"_10pct_R2.fastq"
+
+module load gnu/5.4.0  openmpi/1.10.6  singularity/2.4.0-gnu
+module load pear
+pear -f $SAMPLE_ID"_10pct_R1.fastq -r "$SAMPLE_ID"_10pct_R2.fastq -o "$SAMPLE_ID"_10pct_Peared.fastq"
+
+module load intel impi boost/1.61.0-intel mira
+module load mira
 
 #======================
 #MIRA4 assembly 
 #Create manifest.config for MIRA
-echo -e "\n#manifest file for basic mapping assembly with illumina data using MIRA 4\n\nproject = initial-mapping-of-"$SAMPLE_ID"-to-Ves-mt\njob=genome,mapping,accurate\nparameters = -NW:mrnl=0 -AS:nop=1 SOLEXA_SETTINGS -CO:msr=no -CL:pec=yes\nreadgroup\nis_reference\ndata = $REF_HOME/$REFGENOME\nstrain = Ves-mt-genome\n\nreadgroup = reads\n-CL:pec=yes\ndata = "$RAW_READS_HOME/$SAMPLE_ID"_U0023_10pct.fastq" $RAW_READS_HOME/$SAMPLE_ID"_U0023_10pct.fastq\ntechnology = solexa\nstrain = "$SAMPLE_ID"\n" > $SUPPORT_FILES/$SAMPLE_ID"_manifest.conf"
+echo -e "\n#manifest file for basic mapping assembly with illumina data using MIRA 4\n\nproject = initial-mapping-of-"$SAMPLE_ID"-to-Ves-mt\n\njob=genome,mapping,accurate\n\nparameters = -NW:mrnl=0 -AS:nop=1 SOLEXA_SETTINGS -CO:msr=no -CL:pec=yes\n\nreadgroup\nis_reference\ndata = $REF_HOME/$REFGENOME\nstrain = Ves-mt-genome\n\nreadgroup = reads\ndata = "$RAW_READS_HOME/$SAMPLE_ID"_10pct_Peared.assembled.fastq" $RAW_READS_HOME/$SAMPLE_ID"_10pct_Peared.unassembled.forward.fastq "$SAMPLE_ID"_10pct_Peared.unassembled.reverse.fastq\ntechnology = solexa\nstrain = "$SAMPLE_ID"\n" > $SUPPORT_FILES/$SAMPLE_ID"_manifest.conf"
 
-#Run MIRA  
-module load intel/17.3.191
-module load impi/2017.20160721
-module load intel impi boost/1.61.0-intel mira
-module load mira
+#Run MIRA
+module load mira  
 mira $SUPPORT_FILES/$SAMPLE_ID"_manifest.conf"
 
 #======================
@@ -69,7 +81,7 @@ perl $MITOBIM/MITObim_1.8.pl \
 	-end 10 \
 	-sample $SAMPLE_ID \
 	-ref Ves-mt-genome \
-	-readpool $RAW_READS_HOME/$SAMPLE_ID"_U0023_10pct.fastq" $RAW_READS_HOME/$SAMPLE_ID"_U0023_10pct.fastq" \
+	-readpool $RAW_READS_HOME/$SAMPLE_ID"_10pct_Peared.assembled.fastq" $RAW_READS_HOME/$SAMPLE_ID"_10pct_Peared.unassembled.forward.fastq "$SAMPLE_ID"_10pct_Peared.unassembled.reverse.fastq" \
 	-maf $WORKDIR/$SAMPLE_ID/"initial-mapping-of-"$SAMPLE_ID"-to-Ves-mt_assembly"/"initial-mapping-of-"$SAMPLE_ID"-to-Ves-mt_d_results"/"initial-mapping-of-"$SAMPLE_ID"-to-Ves-mt_out.maf" \
 	&> $SAMPLE_ID".log"
 
